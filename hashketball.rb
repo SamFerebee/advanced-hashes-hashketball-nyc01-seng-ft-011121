c8a447526ba2b8f39fdf434cab9f149dd9b78dac
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-07 20:55:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,7 +199,7 @@ def big_shoe_rebounds
   home_players = game_hash[:home][:players]
   away_players = game_hash[:away][:players]
   all_players = home_players + away_players
-  all_shoes []
+  all_shoes = []
   all_players.each do
     |plyr| all_shoes.push(plyr[:shoe])
   end
